d0b999daa981bcc887d6e515b4e9123e3a8ee985
4PERTURE <38117317+4PERTURE@users.noreply.github.com>
2018-10-14 03:26:59 +0100

    fix building derp
    This fixes building error "error: vendor/xiaomi/msm8953-common: LOCAL_MODULE is not defined."

--- msm8953-common/Android.mk
+++ msm8953-common/Android.mk
@@ -121,6 +121,7 @@ LOCAL_MODULE_SUFFIX := .apk
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := CNEService
 LOCAL_MODULE_OWNER := xiaomi
 LOCAL_SRC_FILES := proprietary/priv-app/CNEService/CNEService.apk
 LOCAL_CERTIFICATE := platform
